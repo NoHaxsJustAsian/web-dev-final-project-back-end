9481aea2385ffe6c53b7aea8e175183891ab54b2
Win Tongtawee <118707963+NoHaxsJustAsian@users.noreply.github.com>
2024-04-20 15:57:27 -0400

    user data schema

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+  }, {
+    timestamps: true,
+  });
+  
+const User = mongoose.model('User', userSchema);
+  
+module.exports = User;
